790c551a979b917b7de994bb97ad5a4cf4288bcb
NathanCheonis05 <guitarman9765@gmail.com>
2025-03-03 18:54:52 -0800

    Update dictionary.py
    I added comments and added the populations to the dictionary.

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,23 @@
 
 Authors: Nathan Cheonis
 
-Description: This file contains a dictionary data structure containing 
+Description: This file contains a dictionary data structure containing populations
+of countries that start with the letter'A'. 
 '''
 
+# The dictionary key is the name of the country and the value is the population of the country
+# The population is an integer value
+
+populations_of_countries={
+    'Afghanistan': 38928346,
+    'Albania': 2877797,
+    'Algeria': 43851044,
+    'Andorra': 77265,
+    'Angola': 32866272,
+    'Antigua and Barbuda': 97928,
+    'Argentina': 45195777,
+    'Armenia': 2963243,
+    'Australia': 25499884,
+    'Austria': 9006398,
+    'Azerbaijan': 10139177
+}
