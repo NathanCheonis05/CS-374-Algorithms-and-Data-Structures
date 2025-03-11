1f1c066ea0fb3ee4a7d309a58e939b3e2ffd6eb5
NathanCheonis05 <guitarman9765@gmail.com>
2025-03-11 12:30:09 -0700

    Update dictionary.py
    Making the class of the dictionary.

--- dictionary.py
+++ dictionary.py
@@ -9,16 +9,12 @@ of countries that start with the letter'A'.
 # The dictionary key is the name of the country and the value is the population of the country
 # The population is an integer value
 
-populations_of_countries={
-    'Afghanistan': 38928346,
-    'Albania': 2877797,
-    'Algeria': 43851044,
-    'Andorra': 77265,
-    'Angola': 32866272,
-    'Antigua and Barbuda': 97928,
-    'Argentina': 45195777,
-    'Armenia': 2963243,
-    'Australia': 25499884,
-    'Austria': 9006398,
-    'Azerbaijan': 10139177
-}
+class populations:
+    def __init__(self, size=10):
+        self.size = size
+        self.data = [None] * size
+    
+    def dict_hash(self, key):
+        return len(key) % self.size
+    
+    
