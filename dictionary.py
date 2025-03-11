1ad46c5acd84eba3e3a309455644d699bdea596b
NathanCheonis05 <guitarman9765@gmail.com>
2025-03-11 16:57:41 -0700

    Update dictionary.py
    I updated the dictionary and I am going to add comments later to this code.

--- dictionary.py
+++ dictionary.py
@@ -2,62 +2,76 @@
 
 Authors: Nathan Cheonis
 
-Description: This file contains a dictionary class that uses . 
+Description: This file contains a dictionary class that uses  . 
 '''
 
 # The dictionary key is the name of the country and the value is the population of the country
 # The population is an integer value
 
-class dictionary:
+class Dictionary:
     def __init__(self, size=10):
         # Initializing the dictionary with empty buckets
         self.size = size
         self.data = [None] * size
     
-    def dict_hash(self, key):
+    def _hash(self, key):
         # Hash function to determine the index of the key
-        return len(key) % self.size
+        return hash(key) % self.size
     
     def set(self, key, value):
         # Inserting a key-value pair into the dictionary
-        index = self.dict_hash(key)
-        
+        index = self._hash(key)
+        start_index = index
         # Using a while loop to handle collisons
         
         while self.data[index] is not None:
-            sorted_key, _ = self.data[index]
-            if sorted_key == key:
+            current_key, _ = self.data[index]
+            if current_key == key:
                 # If the key already exists in the dictionary, update the value
                 self.data[index] = (key, value)
                 return
             index = (index + 1) % self.size
+            if index == start_index:
+                raise Exception("Dictioanry is Full")
         
         self.data[index] = (key, value)
         
     def get(self, key):
         # the get function return the value of a key
-        index = self.dict_hash(key)
+        index = self._hash(key)
+        start_index = index
         
         while self.data[index] is not None:
-            sorted_key, value = self.data[index]
-            if sorted_key == key:
+            current_key, value = self.data[index]
+            if current_key == key:
                 return value
             index = (index + 1) % self.size
-        
-        return None
+            if index == start_index:
+                return None
     
     def delete(self, key):
         # The delete function removes a key-value pair from the dictionary
-        index = self.dict_hash(key)
+        index = self._hash(key)
+        start_index = index
         
         while self.data[index] is not None:
-            sorted_key, _ = self.data[index]
-            if sorted_key == key:
+            current_key, _ = self.data[index]
+            if current_key == key:
                 self.data[index] = None
-                return
+                self._rehash(index)
+                return True
             index = (index + 1) % self.size
+        if index == start_index:
+            return False
         
-        return False
+    def _rehash(self, delete_index):
+        index = (delete_index + 1) % self.size
+        while self.data[index] is not None:
+            key, value = self.data[index]
+            # Remove the key-value pair from the dictionary
+            self.data[index] = None
+            self.set(key, value)
+            index = (index + 1) % self
     
     def display(self):
         # Displays the data stored in the dictionary
