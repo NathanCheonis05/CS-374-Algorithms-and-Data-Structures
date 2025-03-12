f55e2f0475ebbb147783ac15c79b12bcb3069e4c
NathanCheonis05 <guitarman9765@gmail.com>
2025-03-11 23:34:53 -0700

    Deleted dictionary.py
    Hashtable is now the new main file of the project.

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-from dictionary import Dictionary
-
-d = Dictionary()
-
-d.set('name', 'Nate')
-d.set('age', 19)
-d.set('country', 'USA')
-d.set('city', 'Las Vegas')
-d.set('job', 'Professional Sandwich Maker')
-d.set('hobby', 'Music Production')
-
-d.display()
+from 
